fda3046307f3a9931e54dbfb1e6181412c0aa0ca
Harshvardhan <jitug4204@gmail.com>
2023-03-25 20:42:46 +0530

    Minor Changes

--- src/DBUpdate.java
+++ src/DBUpdate.java
@@ -10,7 +10,7 @@ public class DBUpdate extends JFrame{
         setMinimumSize(new Dimension(300,200));
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-//        setResizable(false);
+        setResizable(false);
         setVisible(true);
     }
 }
